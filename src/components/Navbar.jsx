71eaa77c102a50d145eb636b7217c37f095253e4
chefkuna <sukhyun94@naver.com>
2022-11-21 21:26:06 +0900

    Navbar CSS(tailwind)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,12 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <header>
-      <Link to="/">
+    <header className="flex justify-between border-b border-gray-300 p-4">
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
         <Link to="/products/new">
